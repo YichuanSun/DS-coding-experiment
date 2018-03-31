62bd576bc4d5290fdb0fea6e618258b59fb2f072
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-03-31 21:24:24 +0800

    单链表，奇葩头插法

---
+++ 2.单链表.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef struct LinkTable *PtoLT;
+typedef struct LinkTable{
+	int Data;
+	PtoLT Next;
+}LT;
+
+void CreatLinkTable(PtoLT &List);
+void InsertElement(PtoLT &head);
+void DeleteElement(PtoLT &head);
+void SearchbyKey(PtoLT head);
+void Print(PtoLT head);
+void destroyLinkTable(PtoLT &head);
+
+int main()	{
+	int n=1;
+	PtoLT List=(PtoLT)malloc(sizeof(LT));
+	printf("**********************************\n");
+	printf("**	1.创建单链表		**\n");
+	printf("**	2.头插法插入n个元素	**\n");
+	printf("**	3.删除第i个元素		**\n");
+	printf("**	4.按关键字查找单链表	**\n");
+	printf("**	5.计算表长并输出单链表	**\n"); 
+	printf("**	6.销毁单链表		**\n");
+	printf("**********************************\n\n");
+	while (n)	{
+		point:;
+		printf("请输入你的选择：	");
+		scanf("%d",&n);
+		switch(n)	{
+			case 1:	{
+				List=(PtoLT)malloc(sizeof(LT));
+				CreatLinkTable(List);break;
+			}
+			case 2:	InsertElement(List);break;
+			case 3:	DeleteElement(List);break;
+			case 4:	SearchbyKey(List);break;
+			case 5:	Print(List);break;
+			case 6:	destroyLinkTable(List);break;
+			default:	printf("请重新输入!\n");goto point;
+		}
+	}
+	return 0;
+}
+
+void CreatLinkTable(PtoLT &List)	{
+	List->Next=NULL;
+	List->Data=-1;
+	printf("创建成功！\n");
+}
+
+void InsertElement(PtoLT &head)	{
+	int times,t=0;
+	printf("请输入要插入的数据个数：");
+	scanf("%d",&times);
+	while (t++,times--)	{
+		int temp;
+		PtoLT TP=(PtoLT)malloc(sizeof(LT));
+		printf("请输入要插入的第%d个数据：",t);
+		scanf("%d",&temp);
+		TP->Data=temp;
+		TP->Next=head;
+		head=TP;
+	}
+	printf("插入成功！\n");
+}
+
+void DeleteElement(PtoLT &head)	{
+	int i;
+	printf("请输入你要删除的元素序号：");
+	scanf("%d",&i);
+	PtoLT TP=head;
+	while (i-1>0)	
+		i--,TP=TP->Next;
+	PtoLT TP0=TP->Next;
+	TP->Next=TP0->Next;
+	free(TP0);
+	printf("删除成功！\n");
+}
+
+void SearchbyKey(PtoLT head)	{
+	int key,des=0;
+	printf("请输入要查找的关键字：");
+	scanf("%d",&key);
+	while (head!=NULL)	{
+		if (head->Data!=key)
+			++des,head=head->Next;
+		else	break;
+	}
+	if (head!=NULL)
+		printf("位置：%d	数据：%d\n",des,key);
+	else
+		printf("查无此元素！\n");
+}
+
+void Print(PtoLT head)	{
+	if (!head)	{
+		printf("链表为空！\n");
+		return;
+	}
+	int len=0;
+	PtoLT TP=head;
+	while (TP->Next!=NULL)	{
+		printf("%d",TP->Data),len++;
+		if (TP->Next->Next!=NULL)	printf(" ");
+		else printf("\n");
+		TP=TP->Next;
+	}
+	printf("单链表长度：%d\n",len);
+}
+
+void destroyLinkTable(PtoLT &head)	{
+	while (head!=NULL)	{
+		PtoLT T0=head;
+		head=head->Next;
+		free(T0);
+	}
+	printf("销毁成功！\n");
+}
+
+
+
