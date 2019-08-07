cfe6c1bbab2ea085287d3c65543be26c89da2cad
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-07 21:06:50 +0800

    6

--- tree/126.3/main.cpp
+++ tree/126.3/main.cpp
@@ -8,9 +8,19 @@ struct BiTree{
 };
 stack<BiTree*> bs;
 int n,pre[N],in[N];
-void postOrderTraversal(BiTree* t);
-BiTree* preInTree(int pre[],int prel,int prer,int in[],int inl,int inr);
-BiTree* postInTree(int post[],int in[],int root);
+void postOrderTraversal(BiTree* t);     //费递归实现后序遍历
+BiTree* preInTree(int pre[],int prel,int prer,int in[],int inl,int inr);    //这是网上的麻烦套路
+BiTree* preInTree2(int pre[],int in[],int n)  { //这是笔记上的我的方法
+    if (n<=0)   return nullptr;
+    int i=0;
+    while (in[i]!=pre[0])   i++;
+    BiTree* nw=new BiTree;
+    nw->data=in[i];
+    nw->lc=preInTree2(pre+1,in,i);
+    nw->rc=preInTree2(pre+i+1,in+i+1,n-i-1);
+    return nw;
+}
+BiTree* postInTree(int post[],int in[],int root);       //后序中序建树
 void pt(BiTree* ts) {
     if (!ts)    return;
     pt(ts->lc);
@@ -22,7 +32,8 @@ int main()  {
     scanf("%d",&n);
     for (int i=0;i<n;i++)   scanf("%d",&pre[i]);
     for (int i=0;i<n;i++)   scanf("%d",&in[i]);
-    BiTree* root=preInTree(pre,0,n-1,in,0,n-1);
+    //BiTree* root=preInTree(pre,0,n-1,in,0,n-1);
+    BiTree* root=preInTree2(pre,in,n);
     postOrderTraversal(root);
 //    cout<<endl;
 //    pt(root);
@@ -63,16 +74,7 @@ BiTree* preInTree(int pre[],int prel,int prer,int in[],int inl,int inr) {
     nw->rc=preInTree(pre,prel+i-inl+1,prer,in,i+1,inr);
     return nw;
 }
-//int main()  {
-//    int n,pt[35],it[35];
-//    scanf("%d",&n);
-//    for (int i=0;i<n;i++)   scanf("%d",&pt[i]);
-//    for (int i=0;i<n;i++)   scanf("%d",&it[i]);
-//    root=buildTree(n,pt,it);
-//    bfs();
-//    return 0;
-//}
-//
+
 //Node* buildTree(int n,int post[],int inorder[]) {
 //    if (n<=0)   return NULL;
 //    int i=0;
@@ -83,7 +85,3 @@ BiTree* preInTree(int pre[],int prel,int prer,int in[],int inl,int inr) {
 //    newnode->rn=buildTree(n-i-1,post+i,inorder+i+1);
 //    return newnode;
 //}
-
-
-
-
