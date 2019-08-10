d0fe4cc64d43c4e83ab80576255de44dc9ab2d4e
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2019-08-10 22:08:51 +0800

    尝试一下非递归的路径压缩

--- tree/union_find/main.cpp
+++ tree/union_find/main.cpp
@@ -5,8 +5,8 @@ int pa[N],rk[N];
 void initial(int a[],int n)   {
     for (int i=0;i<n;i++)   {a[i]=i;rk[i]=0;}
 }
-void Find(int a[],int x) {
-    while(a[x]!=x)  x=a[x];
+int Find(int a[],int x) {
+    while(a[x]!=x)  a[x]=a[a[x]];       //³¢ÊÔÂ·¾¶Ñ¹Ëõ
     return x;
 }
 void Union(int a[],int p,int q) {
@@ -19,9 +19,26 @@ void Union(int a[],int p,int q) {
     }
     else a[q]=p;
 }
+bool same(int a[],int p,int q){return Find(a,p)==Find(a,q);}
 int main()  {
     int n;
     scanf("%d",&n);
     for (int i=0;i<n;i++)   scanf ("%d",&pa[i]);
+    while (1)   {
+        int is,ts,td,ans;
+        printf("Please input an instruction:\t");
+        scanf("%d",&is);
+        switch(is)  {
+            case 1: {scanf("%d",&ts);ans=Find(pa,ts);printf("%d\n",ans);}break;     //find
+            case 2: {scanf("%d%d",&ts,&td);Union(pa,ts,td);}break;    //Union
+            case 3: td=N;break;
+        }
+        if (td==N)  break;
+    }
     return 0;
 }
+
+
+
+
+
